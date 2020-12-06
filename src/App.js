aff2dc05525d49cfd3d0d881faaccf5a2b94261f
cristiano7988 <cristianooliveiramorales@gmail.com>
2020-12-06 20:33:52 -0300

    Botões

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import GitLogo from "./components/GitLogo"
-import './assets/App.css';
+import GitLogo from "./components/GitLogo";
+import "./assets/App.css";
 
 function App() {
-  return ( 
+  return (
     <section className="main-section">
       <GitLogo />
       <h1>GitSearch</h1>
       <input type="text" placeholder="Pesquisar..." />
+      <div className="btn-container">
+        <button className="btn ver-todos">Ver Todos</button>
+        <button className="btn buscar">Buscar</button>
+      </div>
     </section>
   );
 }
