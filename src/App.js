a958df15268536dcad1f39e1081d8c392f909f00
cristiano7988 <cristianooliveiramorales@gmail.com>
2020-12-06 20:50:00 -0300

    Função que acessa o link passado na task

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import GitLogo from "./components/GitLogo";
 import "./assets/App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <section className="main-section">
-      <GitLogo />
-      <h1>GitSearch</h1>
-      <input type="text" placeholder="Pesquisar..." />
-      <div className="btn-container">
-        <button className="btn ver-todos">Ver Todos</button>
-        <button className="btn buscar">Buscar</button>
-      </div>
-    </section>
-  );
+class App extends Component {
+  verTodos(e) {
+    // const ver = fetch('https://developer.github.com/v3/').then(r=>{r.json()}).then(r=>{console.log(r)});
+  }
+  render() {
+    return (
+      <section className="main-section">
+        <GitLogo />
+        <h1>GitSearch</h1>
+        <input type="text" placeholder="Pesquisar..." />
+        <div className="btn-container">
+          <button className="btn ver-todos" onClick={this.verTodos}>Ver Todos</button>
+          <button className="btn buscar">Buscar</button>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default App;
