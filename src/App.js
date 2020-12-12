b29f9875bd72dacc17b07d7073f4fe9e106c4cd9
cristiano7988 <cristianooliveiramorales@gmail.com>
2020-12-12 13:35:55 -0300

    Refatoração para melhoria na navegação

--- src/App.js
+++ src/App.js
@@ -3,15 +3,43 @@ import { ReactComponent as Back } from "./assets/img/back.svg";
 import "./assets/App.css";
 import { Component } from "react";
 import Lista from "./components/Lista";
+import Perfil from "./components/Perfil";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.lista = [];
-    this.state = {
-      exibirTodos: false,
-      lista: [],
-    };
+  state = {
+    navegacao: false,
+    exibirTodos: false,
+    lista: [],
+    perfil: false,
+    desenvolvedor: "",
+  };
+
+  exibeDesenvolvedor(url) {
+    fetch(url)
+      .then((r) => {
+        if (r.ok) {
+          return r.json();
+        }
+      })
+      .then((r) => {
+        this.setState({
+          exibirTodos: false,
+          perfil: true,
+          navegacao: "Perfil",
+          desenvolvedor: {
+            avatar: r.avatar_url,
+            nome: r.name,
+            login: r.login,
+            seguindo: r.following,
+            projetos: r.public_repos,
+            seguidores: r.followers,
+            bio: r.bio,
+            blog: r.blog,
+            cidade: r.location,
+            url: r.repos_url,
+          },
+        });
+      });
   }
 
   verTodos() {
@@ -23,64 +51,94 @@ class App extends Component {
       })
       .then((desenvolvedores) => {
         if (desenvolvedores) {
-          this.setState({ exibirTodos: true });
-          this.setState({ lista: desenvolvedores });
+          this.setState({
+            exibirTodos: true,
+            lista: desenvolvedores,
+            navegacao: "Lista de usuários",
+          });
         }
       });
   }
 
   retornar() {
-    this.setState({ exibirTodos: false });
+    this.setState({
+      navegacao: false,
+      exibirTodos: false,
+    });
   }
 
   buscar() {
     fetch("https://api.github.com/users")
-    .then((r)=>{
-      if(r.ok) {
-        return r.json();
-      }
-    })
-    .then((r)=>{
-      let lista = r.map((i)=>{return i.login});
-      let urls = r.map((i)=>{return i.url});
-      let input = document.querySelector("#campo-busca").value.toLowerCase();
+      .then((r) => {
+        if (r.ok) {
+          return r.json();
+        }
+      })
+      .then((r) => {
+        let lista = r.map((i) => {
+          return i.login;
+        });
+        let urls = r.map((i) => {
+          return i.url;
+        });
+        let input = document.querySelector("#campo-busca").value.toLowerCase();
 
-      lista.includes(input.toLowerCase()) ?
-        fetch(urls [lista.indexOf(input.toLowerCase())])
-        .then((r)=>{
-          if(r.ok) {
-            return r.json();
-          }
-        })
-        .then((r)=>{
-          // Exibir perfil encontrado
-        })
-      :
-        ''// Exibir mensagem de erro
-    })
+        if (lista.indexOf(input) !== -1) {
+          this.exibeDesenvolvedor(urls[lista.indexOf(input)]);
+        }
+      });
   }
 
   render() {
-    return this.state.exibirTodos ? (
+    return (
       <>
-        <div className="container-titulo">
-          <Back onClick={this.retornar.bind(this)} />
-          <h1>Lista de Usuários</h1>
-        </div>
-        <Lista lista={this.state.lista} />
+        {this.state.navegacao ? (
+          <>
+            <div className="container-titulo">
+              <Back onClick={this.retornar.bind(this)} />
+              <h1>{this.state.navegacao}</h1>
+            </div>
+            {this.state.exibirTodos ? (
+              <Lista
+                exibeDesenvolvedor={this.exibeDesenvolvedor.bind(this)}
+                lista={this.state.lista}
+              />
+            ) : (
+              ""
+            )}
+            {this.state.perfil ? (
+              <Perfil
+                url={this.state.desenvolvedor.url}
+                avatar={this.state.desenvolvedor.avatar}
+                nome={this.state.desenvolvedor.nome}
+                login={this.state.desenvolvedor.login}
+                seguidores={this.state.desenvolvedor.seguidores}
+                projetos={this.state.desenvolvedor.projetos}
+                seguindo={this.state.desenvolvedor.seguindo}
+                bio={this.state.desenvolvedor.bio}
+                blog={this.state.desenvolvedor.blog}
+                cidade={this.state.desenvolvedor.cidade}
+              />
+            ) : (
+              ""
+            )}
+          </>
+        ) : (
+          <section className="main-section">
+            <GitLogo />
+            <h1>GitSearch</h1>
+            <input type="text" placeholder="Pesquisar..." id="campo-busca" />
+            <div className="btn-container">
+              <button className="btn ver-todos" onClick={this.verTodos.bind(this)}>
+                Ver Todos
+              </button>
+              <button className="btn buscar" onClick={this.buscar.bind(this)}>
+                Buscar
+              </button>
+            </div>
+          </section>
+        )}
       </>
-    ) : (
-      <section className="main-section">
-        <GitLogo />
-        <h1>GitSearch</h1>
-        <input type="text" placeholder="Pesquisar..." id="campo-busca" />
-        <div className="btn-container">
-          <button className="btn ver-todos" onClick={this.verTodos.bind(this)}>
-            Ver Todos
-          </button>
-          <button className="btn buscar" onClick={this.buscar.bind(this)}>Buscar</button>
-        </div>
-      </section>
     );
   }
 }
--- src/components/Lista/Lista.jsx
+++ src/components/Lista/Lista.jsx
@@ -1,46 +1,22 @@
 import React, { Component } from "react";
-import Perfil from "../Perfil";
 import "./estilo.css";
 
 class Lista extends Component {
-  state = {
-    exibirLista: true,
-  };
 
-  exibeDesenvolvedor(e) {
-    fetch(e.target.attributes.dados.value)
-      .then((r) => {
-        if (r.ok) {
-          return r.json();
-        }
-      })
-      .then((r) => {
-        this.setState({
-          exibirLista: false,
-          avatar: r.avatar_url,
-          nome: r.name,
-          login: r.login,
-          seguindo: r.following,
-          projetos: r.public_repos,
-          seguidores: r.followers,
-          bio: r.bio,
-          blog: r.blog,
-          cidade: r.location,
-          url: r.repos_url
-        });
-      });
+  _exibeDesenvolvedor(e) {
+    this.props.exibeDesenvolvedor(e.target.dataset.url)
   }
 
   render() {
-    return this.state.exibirLista ? (
+    return (
       <div className="container-lista">
         <input type="text" placeholder="Pesquisar..." />
         {this.props.lista.map((desenvolvedor, index) => {
           return (
             <a
-              dados={desenvolvedor.url}
+              data-url={desenvolvedor.url}
               className="desenvolvedor"
-              onClick={this.exibeDesenvolvedor.bind(this)}
+              onClick={this._exibeDesenvolvedor.bind(this)}
               key={index}
             >
               <div className="container-img">
@@ -51,19 +27,6 @@ class Lista extends Component {
           );
         })}
       </div>
-    ) : (
-      <Perfil
-        url={this.state.url}
-        avatar={this.state.avatar}
-        nome={this.state.nome}
-        login={this.state.login}
-        seguidores={this.state.seguidores}
-        projetos={this.state.projetos}
-        seguindo={this.state.seguindo}
-        bio={this.state.bio}
-        blog={this.state.blog}
-        cidade={this.state.cidade}
-      />
     );
   }
 }
